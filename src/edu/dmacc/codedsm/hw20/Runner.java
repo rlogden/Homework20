d8b9151d7c9b29191d2356a52f72e179a7451d7e
Rachel Ogden <rlogden@dmacc.edu>
2019-04-28 12:49:52 -0500

    Formatting commit

--- src/edu/dmacc/codedsm/hw20/Runner.java
+++ src/edu/dmacc/codedsm/hw20/Runner.java
@@ -9,16 +9,26 @@ public class Runner {
     public static void main(String[] args) {
         List<String> animals = Arrays.asList("Dog", "Cat", "Bird", "Monkey", "Donkey");
 
-        List<Integer> stringLengths = animals.stream().map(str -> str.length()).collect(Collectors.toList());
+        List<Integer> stringLengths = animals.stream()
+                .map(str -> str.length())
+                .collect(Collectors.toList());
         System.out.println("String Lengths: " + stringLengths);
 
-        List flattenedList = animals.stream().map(c -> c.split("")).flatMap(Arrays::stream).collect(Collectors.toList());
+        List flattenedList = animals.stream()
+                .map(c -> c.split(""))
+                .flatMap(Arrays::stream)
+                .collect(Collectors.toList());
         System.out.println("Flattened List: " + flattenedList);
 
-        List<String> dNames = animals.stream().filter(str -> str.charAt(0) == 'D').collect(Collectors.toList());
+        List<String> dNames = animals.stream()
+                .filter(str -> str.charAt(0) == 'D')
+                .collect(Collectors.toList());
         System.out.println("Animals starting with the letter D: " + dNames);
 
-        Integer sumOfLengths = animals.stream().map(str -> str.length()).reduce((element, accumulator) -> element + accumulator).orElse(0);
+        Integer sumOfLengths = animals.stream()
+                .map(str -> str.length())
+                .reduce((element, accumulator) -> element + accumulator)
+                .orElse(0);
         System.out.println(sumOfLengths);
     }
 }
